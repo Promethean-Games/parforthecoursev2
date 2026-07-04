587a542ce3aa3b360591c086358f64931fba27c3
Promethean-Games <alan.acevedo@promethean-games.com>
2026-07-04 07:51:19 -0500

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.parforthecourse"
     compileSdk = 34
 
+    buildFeatures {
+        buildConfig = true
+    }
+
     defaultConfig {
         applicationId = "com.parforthecourse"
         minSdk = 24
