cb38a7bd8c014f18574744cc182e89dce8512db2
Promethean-Games <alan.acevedo@promethean-games.com>
2026-07-08 12:50:21 -0500

    Update API

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.parforthecourse.app"
-    compileSdk = 34
+    compileSdk = 35
 
     buildFeatures {
         buildConfig = true
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.parforthecourse.app"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "3.14"
         buildConfigField("String", "APP_URL", "\"https://promethean-games.github.io/parforthecoursev2/\"")
