62b2e2f79f1d1e1742bd4b9f61674a4280b71cf5
Promethean-Games <alan.acevedo@promethean-games.com>
2026-07-17 05:53:43 -0500

    Upgrade Gradle Plugin

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file for Par for the Course Android app
 
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("com.android.library") version "8.5.2" apply false
+    id("com.android.application") version "8.9.0" apply false
+    id("com.android.library") version "8.9.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
